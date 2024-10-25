f70b0c7ce023cfdff8cfdcd77bdf66b6b7b92c69
유지우 <treeyu12345@naver.com>
2024-10-26 00:12:04 +0900

    FEAT :: Layout 컴포넌트 구현

---
+++ lib/core/layout/eeats_layout.dart
@@ -0,0 +1,36 @@
+import 'package:eeats/core/constants/eeats_style.dart';
+import 'package:flutter/material.dart';
+
+class EeatsLayout extends StatelessWidget {
+  final Color? backgroundColor;
+  final PreferredSizeWidget? appBar;
+  final Widget child;
+  final Widget? floatingActionButton, bottomSheet, bottomNavigationBar;
+  final FloatingActionButtonLocation? floatingActionButtonLocation;
+
+  const EeatsLayout({
+    super.key,
+    required this.child,
+    this.backgroundColor,
+    this.appBar,
+    this.floatingActionButton,
+    this.bottomSheet,
+    this.bottomNavigationBar,
+    this.floatingActionButtonLocation,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backgroundColor ?? EeatsColor.white,
+      appBar: appBar,
+      body: SafeArea(
+        child: child,
+      ),
+      floatingActionButton: floatingActionButton,
+      floatingActionButtonLocation: floatingActionButtonLocation,
+      bottomNavigationBar: bottomNavigationBar,
+      bottomSheet: bottomSheet,
+    );
+  }
+}

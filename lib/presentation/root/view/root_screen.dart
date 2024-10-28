0f88a4f041c3b1c7cbd756fefbc91d9436a8b765
유지우 <treeyu12345@naver.com>
2024-10-28 09:23:44 +0900

    FEAT :: 네이게이터 구현

--- lib/presentation/root/view/root_screen.dart
+++ lib/presentation/root/view/root_screen.dart
@@ -1,9 +1,15 @@
+import 'package:eeats/core/component/eeats_app_bar.dart';
 import 'package:eeats/core/constants/eeats_style.dart';
 import 'package:eeats/core/layout/eeats_layout.dart';
 import 'package:eeats/core/state/navigator_type.dart';
 import 'package:eeats/presentation/home/view/home_screen.dart';
+import 'package:eeats/presentation/my/view/my_screen.dart';
+import 'package:eeats/presentation/root/provider/root_navigator_cubit.dart';
 import 'package:eeats/presentation/root/widget/root_navigator_item.dart';
+import 'package:eeats/presentation/suggest/view/suggest_screen.dart';
+import 'package:eeats/presentation/vote/view/vote_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RootScreen extends StatefulWidget {
   const RootScreen({super.key});
@@ -13,9 +19,16 @@ class RootScreen extends StatefulWidget {
 }
 
 class _RootScreenState extends State<RootScreen> {
+  final List<Widget> pages = [
+    const HomeScreen(),
+    const SuggestScreen(),
+    const VoteScreen(),
+    const MyScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return EeatsLayout(
+      appBar: const EeatsAppBar(),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: 54,
@@ -41,7 +54,17 @@ class _RootScreenState extends State<RootScreen> {
           ),
         ),
       ),
-      child: HomeScreen(),
+      child: BlocBuilder<RootNavigatorCubit, NavigatorType>(
+        builder: (context, state) {
+          return PageView.builder(
+            controller: context.watch<RootNavigatorCubit>().controller,
+            itemCount: NavigatorType.values.length,
+            itemBuilder: (context, index) {
+              return pages[index];
+            },
+          );
+        },
+      ),
     );
   }
 }

8fa07bccece49ca7eda12051b6305f820a3a4dfe
유지우 <treeyu12345@naver.com>
2024-10-28 09:14:46 +0900

    FEAT :: VoteType Controller Cubit 구현

---
+++ lib/presentation/vote/provider/vote_type_controller_cubit.dart
@@ -0,0 +1,26 @@
+import 'package:eeats/core/state/vote_type.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class VoteTypeControllerCubit extends Cubit<VoteType> {
+  VoteTypeControllerCubit() : super(VoteType.open) {
+    controller.addListener(_onPageChanged);
+  }
+
+  final PageController controller = PageController(initialPage: 0);
+
+  void changeType({required VoteType type}) {
+    controller.animateToPage(
+      type.page,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.linearToEaseOut,
+    ).then((_) {
+      emit(type);
+    });
+  }
+
+  void _onPageChanged() {
+    final index = controller.page!.round();
+    emit(VoteType.values.elementAt(index));
+  }
+}

28a64b561c6447b538d498ce8f2cbc3d9eba0913
유지우 <treeyu12345@naver.com>
2024-10-28 09:23:36 +0900

    FEAT :: 네비게이터 Cubit 구현

--- lib/presentation/root/provider/root_navigator_cubit.dart
+++ lib/presentation/root/provider/root_navigator_cubit.dart
@@ -1,12 +1,26 @@
 import 'package:eeats/core/state/navigator_type.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class RootNavigatorCubit extends Cubit<NavigatorType> {
-  RootNavigatorCubit() : super(NavigatorType.home);
+  RootNavigatorCubit() : super(NavigatorType.home) {
+    controller.addListener(_onPageChanged);
+  }
 
-  void changeType({
-    required NavigatorType type,
-  }) {
-    emit(type);
+  final PageController controller = PageController(initialPage: 0);
+
+  void changeType({required NavigatorType type}) {
+    controller.animateToPage(
+      type.index,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.linearToEaseOut,
+    ).then((_) {
+      emit(type);
+    });
+  }
+
+  void _onPageChanged() {
+    final index = controller.page!.round();
+    emit(NavigatorType.values.elementAt(index));
   }
 }

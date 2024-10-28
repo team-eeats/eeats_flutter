ab723f435f353a039ca215dd1f9432847b94509f
유지우 <treeyu12345@naver.com>
2024-10-28 09:15:25 +0900

    FEAT :: VoteTypeControllerCubit을 BlocProvider 설정

--- lib/core/di/eeats_blocs.dart
+++ lib/core/di/eeats_blocs.dart
@@ -1,10 +1,12 @@
 import 'package:eeats/presentation/home/provider/home_meal_controller_cubit.dart';
 import 'package:eeats/presentation/root/provider/root_navigator_cubit.dart';
+import 'package:eeats/presentation/vote/provider/vote_type_controller_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 Future<List<BlocProvider>> blocs() async {
   return [
     BlocProvider<RootNavigatorCubit>(create: (context) => RootNavigatorCubit()),
     BlocProvider<HomeMealControllerCubit>(create: (context) => HomeMealControllerCubit()),
+    BlocProvider<VoteTypeControllerCubit>(create: (context) => VoteTypeControllerCubit()),
   ];
 }

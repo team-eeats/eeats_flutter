187228f399e09e33f6fff33e544139b5dfd0805b
유지우 <treeyu12345@naver.com>
2024-10-28 09:23:01 +0900

    FEAT :: 마이페이지 옵션 위젯 구현

---
+++ lib/presentation/my/widget/my_option_content_widget.dart
@@ -0,0 +1,44 @@
+import 'package:eeats/core/component/eeats_gesture.dart';
+import 'package:eeats/core/constants/eeats_style.dart';
+import 'package:eeats/core/state/help_guide_type.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MyOptionContentWidget extends StatelessWidget {
+  final HelpGuideType type;
+  final Function event;
+
+  const MyOptionContentWidget({
+    super.key,
+    required this.type,
+    required this.event,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return EeatsGesture(
+      event: () => event(),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12),
+        child: Row(
+          children: [
+            SvgPicture.asset(
+              type.icon,
+              colorFilter: const ColorFilter.mode(
+                EeatsColor.main300,
+                BlendMode.srcIn,
+              ),
+            ),
+            const SizedBox(width: 12),
+            Text(
+              type.text,
+              style: EeatsTextStyle.body2(
+                color: EeatsColor.gray800,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

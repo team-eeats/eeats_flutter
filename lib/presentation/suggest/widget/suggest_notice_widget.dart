e553176e33402f369540e72df5e6de1bd726ec17
유지우 <treeyu12345@naver.com>
2024-10-28 09:18:04 +0900

    FEAT :: 건의 페이지 공지사항 구현

---
+++ lib/presentation/suggest/widget/suggest_notice_widget.dart
@@ -0,0 +1,34 @@
+import 'package:eeats/core/constants/eeats_style.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SuggestNoticeWidget extends StatelessWidget {
+  const SuggestNoticeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+        color: EeatsColor.main0,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Padding(
+        padding:
+        const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+        child: Row(
+          children: [
+            SvgPicture.asset("$iconSuggestAsset/speaker_icon.svg"),
+            const SizedBox(width: 10),
+            Text(
+              "급식에 대한 의견을 자유롭게 작성해주세요!",
+              style: EeatsTextStyle.label3(
+                color: EeatsColor.gray800,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

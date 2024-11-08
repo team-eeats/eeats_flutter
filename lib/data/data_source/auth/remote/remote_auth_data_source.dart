0d7721244a95bbe00e75749c7b40d4c6d8e57460
유지우 <treeyu12345@naver.com>
2024-11-08 14:07:23 +0900

    FEAT :: Remote Auth DataSource 구현

---
+++ lib/data/data_source/auth/remote/remote_auth_data_source.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:eeats/core/network/dio/dio.dart';
+import 'package:eeats/data/dto/response/auth/token_response.dart';
+import 'package:eeats/domain/entity/auth/token_entity.dart';
+import 'package:flutter/material.dart';
+
+class RemoteAuthDataSource {
+  static const String endpoint = "/auth";
+
+  Future<TokenEntity> signIn({
+    required String accountId,
+    required String password,
+  }) async {
+    Map<String, dynamic> data = {
+      "account_id": accountId,
+      "password": password,
+    };
+    try {
+      final response = await dio.post(
+        "$endpoint/user-login",
+        data: data,
+      );
+
+      return TokenResponse.fromJson(response.statusCode!, response.data).toEntity();
+    } catch (err) {
+      throw Exception(err.toString());
+    }
+  }
+
+  Future<TokenEntity> reIssue({
+    required String refreshToken,
+  }) async {
+    try {
+      final response = await dio.put(
+        "$endpoint/reissue",
+        options: Options(
+          headers: {
+            "Refresh-Token": refreshToken,
+          },
+        ),
+      );
+
+      return TokenResponse.fromJson(response.statusCode!, response.data).toEntity();
+    } catch (err) {
+      throw Exception(err.toString());
+    }
+  }
+}

668e2e0c3b6e0c8b1625fe871f969b9717c989bd
유지우 <treeyu12345@naver.com>
2024-11-11 19:34:05 +0900

    FEAT :: 건의 삭제 이벤트 핸들러 추가

--- lib/presentation/suggest/bloc/my_suggest/my_suggest_bloc.dart
+++ lib/presentation/suggest/bloc/my_suggest/my_suggest_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:eeats/domain/entity/suggest/suggest_list_entity.dart';
-import 'package:eeats/domain/use_case/suggest/edit_suggest_use_case.dart';
+import 'package:eeats/domain/use_case/suggest/delete_my_suggest_use_case.dart';
+import 'package:eeats/domain/use_case/suggest/edit_my_suggest_use_case.dart';
 import 'package:eeats/domain/use_case/suggest/get_my_suggest_use_case.dart';
 import 'package:eeats/presentation/suggest/bloc/my_suggest/my_suggest_event.dart';
 import 'package:eeats/presentation/suggest/bloc/my_suggest/my_suggest_state.dart';
@@ -8,13 +9,16 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class MySuggestBloc
     extends Bloc<MySuggestEvent, MySuggestState<SuggestListEntity>> {
   final GetMySuggestUseCase _getMySuggestUseCase;
-  final EditSuggestUseCase _editSuggestUseCase;
+  final EditMySuggestUseCase _editMySuggestUseCase;
+  final DeleteMySuggestUseCase _deleteMySuggestUseCase;
 
-  MySuggestBloc(
-      {required GetMySuggestUseCase getMySuggestUseCase,
-      required EditSuggestUseCase editSuggestUseCase})
-      : _getMySuggestUseCase = getMySuggestUseCase,
-        _editSuggestUseCase = editSuggestUseCase,
+  MySuggestBloc({
+    required GetMySuggestUseCase getMySuggestUseCase,
+    required EditMySuggestUseCase editMySuggestUseCase,
+    required DeleteMySuggestUseCase deleteMySuggestUseCase,
+  })  : _getMySuggestUseCase = getMySuggestUseCase,
+        _editMySuggestUseCase = editMySuggestUseCase,
+        _deleteMySuggestUseCase = deleteMySuggestUseCase,
         super(
           Initial(
             data: SuggestListEntity(
@@ -25,6 +29,7 @@ class MySuggestBloc
         ) {
     on<GetMySuggestEvent>(_getMySuggestEventHandler);
     on<EditMySuggestEvent>(_editMySuggestEventHandler);
+    on<DeleteMySuggestEvent>(_deleteMySuggestEventHandler);
   }
 
   void _getMySuggestEventHandler(
@@ -51,9 +56,33 @@ class MySuggestBloc
     try {
       emit(Loading(data: state.value));
 
-      final response = await _editSuggestUseCase.execute(
+      final response = await _editMySuggestUseCase.execute(
         title: event.title,
         content: event.content,
+        uuid: event.uuid,
+      );
+
+      if (response == 204) {
+        final value = await _getMySuggestUseCase.execute();
+        if (value.statusCode == 200) {
+          emit(Success(data: value));
+        } else {
+          emit(Failure(message: response));
+        }
+      }
+    } catch (err) {
+      emit(Failure(message: err.toString()));
+    }
+  }
+
+  void _deleteMySuggestEventHandler(
+    DeleteMySuggestEvent event,
+    Emitter<MySuggestState<SuggestListEntity>> emit,
+  ) async {
+    try {
+      emit(Loading(data: state.value));
+
+      final response = await _deleteMySuggestUseCase.execute(
         uuid: event.uuid,
       );
 

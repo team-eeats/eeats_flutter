0380b70d0ca7deb1d7960292ae02d2756ddf6a19
유지우 <treeyu12345@naver.com>
2024-11-11 19:22:35 +0900

    FEAT :: 건의 수정 이벤트 추가

--- lib/presentation/suggest/bloc/my_suggest/my_suggest_event.dart
+++ lib/presentation/suggest/bloc/my_suggest/my_suggest_event.dart
@@ -1,3 +1,13 @@
 abstract class MySuggestEvent {}
 
 class GetMySuggestEvent extends MySuggestEvent {}
+
+class EditMySuggestEvent extends MySuggestEvent {
+  final String title, content, uuid;
+
+  EditMySuggestEvent({
+    required this.title,
+    required this.content,
+    required this.uuid,
+  });
+}

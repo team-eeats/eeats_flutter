ee0ef07cda38b93755083fb283565ebef6669700
유지우 <treeyu12345@naver.com>
2024-11-11 19:22:50 +0900

    FEAT :: 건의 수정 Event Handler 구현

--- lib/presentation/suggest/bloc/my_suggest/my_suggest_bloc.dart
+++ lib/presentation/suggest/bloc/my_suggest/my_suggest_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:eeats/domain/entity/suggest/suggest_list_entity.dart';
+import 'package:eeats/domain/use_case/suggest/edit_suggest_use_case.dart';
 import 'package:eeats/domain/use_case/suggest/get_my_suggest_use_case.dart';
 import 'package:eeats/presentation/suggest/bloc/my_suggest/my_suggest_event.dart';
 import 'package:eeats/presentation/suggest/bloc/my_suggest/my_suggest_state.dart';
@@ -7,10 +8,13 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class MySuggestBloc
     extends Bloc<MySuggestEvent, MySuggestState<SuggestListEntity>> {
   final GetMySuggestUseCase _getMySuggestUseCase;
+  final EditSuggestUseCase _editSuggestUseCase;
 
-  MySuggestBloc({
-    required GetMySuggestUseCase getMySuggestUseCase,
-  })  : _getMySuggestUseCase = getMySuggestUseCase,
+  MySuggestBloc(
+      {required GetMySuggestUseCase getMySuggestUseCase,
+      required EditSuggestUseCase editSuggestUseCase})
+      : _getMySuggestUseCase = getMySuggestUseCase,
+        _editSuggestUseCase = editSuggestUseCase,
         super(
           Initial(
             data: SuggestListEntity(
@@ -20,6 +24,7 @@ class MySuggestBloc
           ),
         ) {
     on<GetMySuggestEvent>(_getMySuggestEventHandler);
+    on<EditMySuggestEvent>(_editMySuggestEventHandler);
   }
 
   void _getMySuggestEventHandler(
@@ -33,6 +38,32 @@ class MySuggestBloc
 
       if (response.statusCode == 200) {
         emit(Success(data: response));
+      }
+    } catch (err) {
+      emit(Failure(message: err.toString()));
+    }
+  }
+
+  void _editMySuggestEventHandler(
+    EditMySuggestEvent event,
+    Emitter<MySuggestState<SuggestListEntity>> emit,
+  ) async {
+    try {
+      emit(Loading(data: state.value));
+
+      final response = await _editSuggestUseCase.execute(
+        title: event.title,
+        content: event.content,
+        uuid: event.uuid,
+      );
+
+      if (response == 204) {
+        final value = await _getMySuggestUseCase.execute();
+        if (value.statusCode == 200) {
+          emit(Success(data: value));
+        } else {
+          emit(Failure(message: response));
+        }
       }
     } catch (err) {
       emit(Failure(message: err.toString()));

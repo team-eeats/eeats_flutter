c08e8c34906d963527e926a661dec030e9cd270f
유지우 <treeyu12345@naver.com>
2024-11-12 09:26:05 +0900

    FEAT :: Bloc 등록

--- lib/core/di/eeats_blocs.dart
+++ lib/core/di/eeats_blocs.dart
@@ -4,14 +4,17 @@ import 'package:eeats/data/data_source/auth/remote/remote_auth_data_source.dart'
 import 'package:eeats/data/data_source/meal/remote/remote_meal_data_source.dart';
 import 'package:eeats/data/data_source/notice/remote/remote_notice_data_source.dart';
 import 'package:eeats/data/data_source/suggest/remote/remote_suggest_data_source.dart';
+import 'package:eeats/data/data_source/user/remote/remote_user_data_source.dart';
 import 'package:eeats/data/repository/auth/auth_repository_impl.dart';
 import 'package:eeats/data/repository/meal/meal_repository_impl.dart';
 import 'package:eeats/data/repository/notice/notice_repository_impl.dart';
 import 'package:eeats/data/repository/suggest/suggest_repository_impl.dart';
+import 'package:eeats/data/repository/user/user_repository_impl.dart';
 import 'package:eeats/domain/repository/auth/auth_repository.dart';
 import 'package:eeats/domain/repository/meal/meal_repository.dart';
 import 'package:eeats/domain/repository/notice/notice_repository.dart';
 import 'package:eeats/domain/repository/suggest/suggest_repository.dart';
+import 'package:eeats/domain/repository/user/user_repository.dart';
 import 'package:eeats/domain/use_case/auth/re_issue_use_case.dart';
 import 'package:eeats/domain/use_case/auth/sign_in_use_case.dart';
 import 'package:eeats/domain/use_case/meal/get_meal_use_case.dart';
@@ -22,9 +25,14 @@ import 'package:eeats/domain/use_case/suggest/edit_my_suggest_use_case.dart';
 import 'package:eeats/domain/use_case/suggest/get_my_suggest_use_case.dart';
 import 'package:eeats/domain/use_case/suggest/get_suggest_list_use_case.dart';
 import 'package:eeats/domain/use_case/suggest/post_suggest_use_case.dart';
+import 'package:eeats/domain/use_case/user/get_my_use_case.dart';
+import 'package:eeats/domain/use_case/user/patch_allergy_use_case.dart';
+import 'package:eeats/domain/use_case/user/patch_profile_use_case.dart';
 import 'package:eeats/presentation/home/bloc/home_bloc.dart';
 import 'package:eeats/presentation/my/bloc/notice/notice_bloc.dart';
 import 'package:eeats/presentation/my/bloc/notice_detail/notice_detail_bloc.dart';
+import 'package:eeats/presentation/my/bloc/user/my_bloc.dart';
+import 'package:eeats/presentation/my/cubit/select_allergy_cubit.dart';
 import 'package:eeats/presentation/sign_in/bloc/sign_in_bloc.dart';
 import 'package:eeats/presentation/splash/bloc/splash_bloc.dart';
 import 'package:eeats/presentation/suggest/bloc/my_suggest/my_suggest_bloc.dart';
@@ -36,6 +44,7 @@ Future<List<BlocProvider>> blocs() async {
   RemoteMealDataSource remoteMealDataSource = RemoteMealDataSource();
   RemoteSuggestDataSource remoteSuggestDataSource = RemoteSuggestDataSource();
   RemoteNoticeDataSource remoteNoticeDataSource = RemoteNoticeDataSource();
+  RemoteUserDataSource remoteUserDataSource = RemoteUserDataSource();
 
   AuthRepository authRepository =
       AuthRepositoryImpl(remoteAuthDataSource: remoteAuthDataSource);
@@ -45,6 +54,8 @@ Future<List<BlocProvider>> blocs() async {
       SuggestRepositoryImpl(remoteSuggestDataSource: remoteSuggestDataSource);
   NoticeRepository noticeRepository =
       NoticeRepositoryImpl(remoteNoticeDataSource: remoteNoticeDataSource);
+  UserRepository userRepository =
+      UserRepositoryImpl(remoteUserDataSource: remoteUserDataSource);
 
   SignInUseCase signInUseCase = SignInUseCase(authRepository: authRepository);
   ReIssueUseCase reIssueUseCase =
@@ -69,11 +80,18 @@ Future<List<BlocProvider>> blocs() async {
   GetNoticeDetailUseCase getNoticeDetailUseCase =
       GetNoticeDetailUseCase(noticeRepository: noticeRepository);
 
+  GetMyUseCase getMyUseCase = GetMyUseCase(userRepository: userRepository);
+  PatchAllergyUseCase patchAllergyUseCase =
+      PatchAllergyUseCase(userRepository: userRepository);
+  PatchProfileUseCase patchProfileUseCase =
+      PatchProfileUseCase(userRepository: userRepository);
+
   return [
     BlocProvider<TextFieldFocusCubit>(
         create: (context) => TextFieldFocusCubit()),
     BlocProvider<TextFieldEmptyCubit>(
         create: (context) => TextFieldEmptyCubit()),
+    BlocProvider<SelectAllergyCubit>(create: (context) => SelectAllergyCubit()),
     BlocProvider<SignInBloc>(
         create: (context) => SignInBloc(signInUseCase: signInUseCase)),
     BlocProvider<SplashBloc>(
@@ -100,7 +118,13 @@ Future<List<BlocProvider>> blocs() async {
       create: (context) => MySuggestBloc(
         getMySuggestUseCase: getMySuggestUseCase,
         editMySuggestUseCase: editMySuggestUseCase,
-        deleteMySuggestUseCase: deleteMySuggestUseCase
+        deleteMySuggestUseCase: deleteMySuggestUseCase,
+      ),
+    ),
+    BlocProvider<MyBloc>(
+      create: (context) => MyBloc(
+        getMyUseCase: getMyUseCase,
+        patchProfileUseCase: patchProfileUseCase,
       ),
     ),
   ];
